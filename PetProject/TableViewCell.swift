dff38bb3072879cadedbaaaa35e3dc9347668ee4
Sergey <kolesnikov.sergey.al@gmail.com>
2022-06-17 17:16:50 +0300

    1. Add NB 2. Add TableViewController 3. Setting navigation items

---
+++ PetProject/TableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TableViewCell.swift
+//  PetProject
+//
+//  Created by катя on 17.06.2022.
+//
+
+import Foundation
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+}
+
--- PetProject/ViewController.swift
+++ PetProject/ViewController.swift
@@ -7,11 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    let betaArray = ["a", "b", "c"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        self.tableView.register(TableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        setupNavigationBar()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        betaArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        var content = cell.defaultContentConfiguration()
+
+        content.text = betaArray[indexPath.row]
+        cell.contentConfiguration = content
+
+        return cell
+    }
+    
+    
+    private func setupNavigationBar() {
+        title = "NONE"
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let navBarAppearance = UINavigationBarAppearance()
+        
+        navBarAppearance.backgroundColor = UIColor(
+            red: 10/255,
+            green: 15/255,
+            blue: 20/255,
+            alpha: 195/255
+        )
+        
+        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        navigationController?.navigationBar.standardAppearance = navBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+        
     }
 
 
